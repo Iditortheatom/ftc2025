e019e707881d3bfe33b61e72b22918fb5b50ec1f
Isidor <isidor.herea@gmail.com>
2025-12-29 14:16:04 +0200

    am facut cv poate e ok idfk nu mai vreau sa traiesc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpp.java
@@ -1,28 +1,15 @@
-//4 dc sasiu - clasic
-//dc maturica power 1 0
-//dc spinner (revolver) PID cu 3 limitswich uri sincronizat cu senzorii de culaore
-//la tureta dc care contrealaza pivotu
-//motor de turatie Dc
-//ajustare unghi servo
-//servo jos eject spinner range 150 grade
-//servo sus ajustare unghi tureta
-//
-//pozitii:
-//intake activ (dc maturica activ -> toate slot urile sunt ocupate +stiind ordinea)
-//scoring(ajustare unghi servo-> se duce in pozitie , pivotu->urmareste cosul prin limelight ,
-// motor turatie (pornit putin mai devreme , invartit constant la viteza mai mica 0.6,
-// cand is 3 slot uri pline dau putere 1) -> activ , eject bila mutat spinner.
-
-
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public abstract class sasiu extends LinearOpMode{
+@TeleOp(name = "StefanAtingeMinori")
+public class TeleOpp extends LinearOpMode {
+
     DcMotor leftFront;
     DcMotor leftBack;
     DcMotor rightFront;
@@ -31,8 +18,19 @@ public abstract class sasiu extends LinearOpMode{
     DcMotor pivoter;
     DcMotor turatie;
     DcMotor spinner;
+
+    DcMotor tureta;
+    Servo eject;
+    TouchSensor touch;
+
+    int ticksPerRevolution = 0; // de luat de la hard
+    double gearRatio = 1.0;
+    double degreesToRotate = 120;
+    boolean reset2 = false;
+
     Servo unghi;
     TouchSensor limitswitch;
+
     int position = 0;
     int encoderPosition = 0;
     int targetPositionSpinner = 0;
@@ -53,7 +51,7 @@ public abstract class sasiu extends LinearOpMode{
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         leftBack = hardwareMap.dcMotor.get("leftRear");
-        rightBack= hardwareMap.dcMotor.get("rightRear");
+        rightBack = hardwareMap.dcMotor.get("rightRear");
 
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
@@ -62,14 +60,26 @@ public abstract class sasiu extends LinearOpMode{
     }
 
     private void InitDc() {
-        matura =hardwareMap.dcMotor.get("matura");
-        pivoter =hardwareMap.dcMotor.get("pivoter");
-        turatie =hardwareMap.dcMotor.get("turatie");
+        matura = hardwareMap.dcMotor.get("matura");
+        pivoter = hardwareMap.dcMotor.get("pivoter");
+        turatie = hardwareMap.dcMotor.get("turatie");
         spinner = hardwareMap.dcMotor.get("revolver");
+
+        tureta = hardwareMap.dcMotor.get("tureta");
+        tureta.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        tureta.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        tureta.setDirection(DcMotor.Direction.FORWARD);
     }
 
     private void InitServo() {
         unghi = hardwareMap.servo.get("unghi tureta");
+        eject = hardwareMap.servo.get("eject");
+    }
+
+    private void InitSensors() {
+        limitswitch = hardwareMap.get(TouchSensor.class, "limitSwitch");
+
+        touch = hardwareMap.get(TouchSensor.class, "limitSwitch");
     }
 
     @Override
@@ -77,17 +87,52 @@ public abstract class sasiu extends LinearOpMode{
         InitWheels();
         InitDc();
         InitServo();
+        InitSensors();
+
+        waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             gamepadSetup();
             SetWheelsPower();
 
+            if (gamepad1.a) { // de pus butonul
+                eject.setDirection(Servo.Direction.FORWARD);
+                eject.setPosition(0.55);
+                eject.setPosition(0.0);
+
+                int targetTicks = (int) ((degreesToRotate / 360.0)
+                        * ticksPerRevolution * gearRatio);
+
+                tureta.setTargetPosition(targetTicks);
+                tureta.setPower(0.5);
+
+                while (opModeIsActive() && tureta.isBusy()) {
+                    telemetry.addData("Motor Ticks", tureta.getCurrentPosition());
+                    telemetry.update();
+                }
+                tureta.setPower(0);
+            }
+
+            if (gamepad1.b) { // de pus butonul
+                reset2 = true;
+            }
+
+            if (reset2) {
+                tureta.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                tureta.setPower(1);
+                if (touch.isPressed()) {
+                    tureta.setPower(0);
+                    reset2 = false;
+                    tureta.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    tureta.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                }
+            }
         }
     }
 
     private void SetWheelsPower() {
         double left_x = gamepad1.left_stick_x;
-        double left_y = -gamepad1.left_stick_y; // forward is negative
+        double left_y = -gamepad1.left_stick_y;
         double right_x = gamepad1.right_stick_x;
 
         double front_left_pw = left_y + left_x + right_x;
@@ -111,6 +156,7 @@ public abstract class sasiu extends LinearOpMode{
         rightFront.setPower(front_right_pw);
         rightBack.setPower(back_right_pw);
     }
+
     private void intake() {
         matura.setPower(1.0);
     }
@@ -118,21 +164,18 @@ public abstract class sasiu extends LinearOpMode{
     private void revolver() {
         ElapsedTime timer = new ElapsedTime();
         timer.startTime();
-        while (timer.seconds()>3 && opModeIsActive()) {
+        while (timer.seconds() > 3 && opModeIsActive()) {
             while (limitswitch.isPressed()) {
                 encoderPosition = spinner.getCurrentPosition();
                 error = targetPositionSpinner - encoderPosition;
-
 
                 derivative = (error - lastError) / timer.seconds();
                 integralSum = integralSum + (error * timer.seconds());
 
                 out = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
-
                 spinner.setPower(out);
 
                 lastError = error;
-
                 timer.reset();
             }
         }
@@ -142,26 +185,23 @@ public abstract class sasiu extends LinearOpMode{
         unghi.setPosition(unghiTargetPosition / 270);
 
         ElapsedTime timer = new ElapsedTime();
-        while(pivoterTargetPosition - pivoter.getCurrentPosition() != 0) {
+        while (pivoterTargetPosition - pivoter.getCurrentPosition() != 0) {
             error = pivoterTargetPosition - pivoter.getCurrentPosition();
-
 
             derivative = (error - lastError) / timer.seconds();
             integralSum = integralSum + (error * timer.seconds());
 
             out = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
-
             pivoter.setPower(out);
 
             lastError = error;
-
             timer.reset();
         }
     }
 
-    private void turatie(){
+    private void turatie() {
         turatie.setPower(0.6);
-        if(slotIndex == 3){
+        if (slotIndex == 3) {
             turatie.setPower(1.0);
         }
     }
@@ -181,6 +221,5 @@ public abstract class sasiu extends LinearOpMode{
         boolean d = gamepad1.dpad_right;
         boolean st = gamepad1.dpad_left;
         boolean j = gamepad1.dpad_down;
-
     }
 }
